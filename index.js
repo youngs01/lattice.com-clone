10fb6e707d7a5b6c75959bf21d3ae07729ce20ee
shamroz khan <124503790+youngs01@users.noreply.github.com>
2023-12-01 04:01:59 -0800

    hi

---
+++ index.js
@@ -0,0 +1,14 @@
+
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const activateModalBtn = document.querySelector('.plateform-activate-modal');
+    const modalNavbar = document.querySelector('.modal-platform-navbar');
+
+    // Add click event listener to the activate button
+    activateModalBtn.addEventListener('click', function() {
+        // Toggle the visibility of the modal navbar
+        modalNavbar.classList.toggle('active');
+    });
+});
+  
+
