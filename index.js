abd8fae70e29cfa8559aacfb5ca94e7879772746
shamroz khan <124503790+youngs01@users.noreply.github.com>
2023-12-05 01:32:03 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const activateModalBtn = document.querySelector('.plateform-activate-modal');
+  const modalNavbar = document.querySelector('.modal-platform-navbar');
 
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    const activateModalBtn = document.querySelector('.plateform-activate-modal');
-    const modalNavbar = document.querySelector('.modal-platform-navbar');
-
-    // Add click event listener to the activate button
-    activateModalBtn.addEventListener('click', function() {
-        // Toggle the visibility of the modal navbar
-        modalNavbar.classList.toggle('active');
-    });
+  // Add click event listener to the activate button
+  activateModalBtn.addEventListener('click', function() {
+      // Toggle the visibility of the modal navbar
+      modalNavbar.classList.toggle('active');
+  });
 });
-  
 
+  
